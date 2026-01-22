93cccf7818d501f1532cf72dbf9587c6a26778b2
roudra323 <asirroudra@gmail.com>
2026-01-22 19:36:10 +0600

    fixed unnecessary rerenders

--- src/providers/Web3Provider.tsx
+++ src/providers/Web3Provider.tsx
@@ -2,18 +2,19 @@
 
 import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 import { WagmiProvider } from "wagmi";
 import { config } from "@/config/wagmi";
 
 import "@rainbow-me/rainbowkit/styles.css";
-
-const queryClient = new QueryClient();
 
 interface Web3ProviderProps {
   children: React.ReactNode;
 }
 
 export function Web3Provider({ children }: Web3ProviderProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
