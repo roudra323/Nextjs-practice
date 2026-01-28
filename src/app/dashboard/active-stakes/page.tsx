860c40f1aa426abe4d240a9db0ee65dc7bf873fc
roudra323 <asirroudra@gmail.com>
2026-01-28 14:30:36 +0600

    refactor: cleaned up same files

--- src/app/dashboard/active-stakes/page.tsx
+++
@@ -1,9 +0,0 @@
-import ActiveStakes from "@/components/validator/ActiveStakes";
-
-export default function ActiveStakesPage() {
-  return (
-    <div className="flex flex-col gap-4 sm:gap-6 p-4 sm:p-5">
-      <ActiveStakes />
-    </div>
-  );
-}
